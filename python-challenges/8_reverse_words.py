288b8a1a39be591f35b19dee2c43a66d15f3c764
duddles <duddles@users.noreply.github.com>
2015-10-28 11:48:48 -0500

    Update 8_reverse_words.py

--- python-challenges/8_reverse_words.py
+++ python-challenges/8_reverse_words.py
@@ -1,11 +1,10 @@
 import sys
 
 def print_reversed(line):
-    if len(line) == 0:
-        return
-    split = line.split(' ')
-    reversed_split = split[::-1]
-    print ' '.join(reversed_split)
+    if line:
+        split = line.split(' ')
+        reversed_split = split[::-1]
+        print ' '.join(reversed_split)
 
 if __name__ == "__main__":
     for line in open(sys.argv[1]):
